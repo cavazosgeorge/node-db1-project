203a4ebad2e50d79939b28fef84584a6b206a142
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-01 22:11:55 -0400

    GET by id: fully working

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,4 +1,5 @@
 // MIDDLEWARE
+const Account = require("./accounts-model");
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
@@ -7,13 +8,25 @@ exports.checkAccountPayload = (req, res, next) => {
 };
 
 exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
   console.log("checkAccountNameUnique Middleware");
   next();
 };
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log("checkAccountId Middleware");
-  next();
+exports.checkAccountId = async (req, res, next) => {
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      next({
+        success: false,
+        status: 404,
+        message: "not found",
+      });
+      // ACCOUNT EXITS => ATTACH TO REQ-BODY AND CONTINUE ON =>
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 };
