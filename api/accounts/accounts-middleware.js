27dd3502d4805888c915f0b833c40d8e3fc79ca6
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-01 23:12:20 -0400

    Middleware: checkAccountPayload funcional

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,17 +1,38 @@
 // MIDDLEWARE
 const Account = require("./accounts-model");
 
+// MIDDLEWARE #1
 exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log("checkAccountPayload Middleware");
-  next();
+  const error = { staus: 400 };
+  const { name, budget } = req.body;
+
+  // CONDITIONAL LOGIC
+  if (name === undefined || budget === undefined) {
+    error.message = "name and budget are required";
+  } else if (typeof name !== "string") {
+    error.message = "name of account must be a string";
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100";
+  } else if (typeof budget !== "number" || isNaN(budget)) {
+    error.message = "budget of account must be a number";
+  } else if (budget < 0 || budget > 1000000) {
+    error.message = "budget of account is too large or too small";
+  }
+
+  if (error.message) {
+    next(error);
+  } else {
+    next();
+  }
 };
 
+// MIDDLEWARE #2
 exports.checkAccountNameUnique = (req, res, next) => {
   console.log("checkAccountNameUnique Middleware");
   next();
 };
 
+// MIDDLEWARE #3
 exports.checkAccountId = async (req, res, next) => {
   try {
     const account = await Account.getById(req.params.id);
